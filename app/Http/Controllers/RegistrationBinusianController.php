a05cac5423991474b07628fb9c3f6489725cb3ef
LouisETD <139093983+LouisETD@users.noreply.github.com>
2024-02-02 14:11:59 +0700

    Add files via upload

---
+++ app/Http/Controllers/RegistrationBinusianController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationBinusianController extends Controller
+{
+    public function registrationBinusian() {
+        return view('registBinusian');
+    }
+}
---
+++ app/Http/Controllers/RegistrationNonBinusianController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationNonBinusianController extends Controller
+{
+    public function registrationNonBinusian() {
+        return view('registNonBinusian');
+    }
+}
