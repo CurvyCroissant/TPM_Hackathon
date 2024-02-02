f31aaa0e23404161dba96477f272bc317a7747ec
Aaron Nathanael <aaronnathanaelishakleman@gmail.com>
2024-02-02 13:26:53 +0700

    Moved Images Folder

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,29 @@ use App\Http\Controllers\RegistrationBinusianController;
 use App\Http\Controllers\RegistrationNonBinusianController;
 use App\Http\Controllers\AdminPanelController;
 
+// HOME
 Route::get('/', [HomeController::class, 'home']);
 
+
+// DASHBOARD
 Route::get('/dashboard', [DashboardController::class, 'dashboard']);
 
+
+// LOGIN
 Route::get('/login', [LoginController::class, 'login']);
 
+
+// TEAM REGISTRATION
 Route::get('/registration', [RegistrationController::class, 'registration']);
 
+
+// BINUSIAN REGISTRATION
 Route::get('/registration-binusian', [RegistrationBinusianController::class, 'registrationBinusian']);
 
+
+// NON-BINUSIAN REGISTRATION
 Route::get('/registration-non-binusian', [RegistrationNonBinusianController::class, 'registrationNonBinusian']);
 
+
+// ADMIN PANEL
 Route::get('/admin-panel', [AdminPanelController::class, 'adminPanel']);
